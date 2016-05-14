b7dd4353c0897ec62827391967d088058ea01064
George <gj.successfulmillionaire@gmail.com>
2016-05-15 08:27:28 +1200

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -32,7 +32,7 @@ class BetterSmoochApiBot extends SmoochApiBot {
 }
 
 const name = 'SmoochBot';
-const avatarUrl = 'https://s.gravatar.com/avatar/f91b04087e0125153623a3778e819c0a?s=80';
+const avatarUrl = 'http://postimg.org/image/u0b5gtxbl/';
 const store = new SmoochApiStore({
     jwt
 });
